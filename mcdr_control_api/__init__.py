2538b032c717a7c28d916f2cb022f2c56b3ca7e0
Mooling0602 <clemooling@outlook.com>
2025-05-23 09:39:49 +0800

    feat: init src codes

---
+++ mcdr_control_api/__init__.py
@@ -0,0 +1,97 @@
+"""mcdr_control_api
+
+A simple API for controlling mcdreforged server.
+"""
+from enum import StrEnum, auto
+from fastapi import FastAPI, Query
+from mcdreforged.api.types import PluginServerInterface
+from mcdreforged.constants.core_constant import VERSION as mcdr_ver
+
+
+app = FastAPI()
+app_id: str | None = None
+psi: PluginServerInterface | None = None
+
+
+class Path(StrEnum):
+    """The target you may want to query for.
+    """
+    PLUGIN = auto()
+    MCDR = auto()
+
+
+@app.get("/test")
+async def test():
+    """A test endpoint to check if this FastAPI instance(app) is working fine.
+
+    If so, it will return a string `"Hello, world!"`.
+    """
+    return "Hello, world!"
+
+
+@app.get("/app_id")
+async def get_app_id():
+    """Return the app_id of this FastAPI instance.
+
+    If app works fine, it will return a string of its `app_id`.
+    """
+    if app_id:
+        return app_id
+    else:
+        return {"error": "app not inited!"}
+
+
+@app.get("/query/{path}/{search_key}")
+async def query(
+    path: Path,
+    search_key: str,
+    other: str | None = Query(default=None)
+):
+    """Query for the information of the MCDReforged server.
+
+    
+    """
+    if not isinstance(psi, PluginServerInterface):
+        raise RuntimeError("MCDR plugin server interface not initialized!")
+    match path:
+        case Path.PLUGIN:
+            match search_key:
+                case "ver" | "version":
+                    if other is None:
+                        return str(
+                            psi.get_self_metadata().version)
+        case Path.MCDR:
+            match search_key:
+                case "ver" | "version":
+                    if other is None:
+                        return str(mcdr_ver)
+                case "status":
+                    if other is None:
+                        return {
+                            "version": str(mcdr_ver),
+                            "is_running": True,
+                            "is_server_running": psi.is_server_running(),
+                            "is_server_startup": psi.is_server_startup(),
+                            "is_rcon_running": psi.is_rcon_running(),
+                            "plugin_count": len(psi.get_plugin_list()),
+                            "server_pid": psi.get_server_pid(),
+                            "server_version": psi.get_server_information().version
+                        }
+                    else:
+                        match other:
+                            case "ver" | "version":
+                                return str(mcdr_ver)
+                            case "is_running":
+                                return True
+                            case "is_server_running":
+                                return psi.is_server_running()
+                            case  "is_server_startup":
+                                return psi.is_server_startup()
+                            case  "is_rcon_running":
+                                return psi.is_rcon_running()
+                            case "plugin_count":
+                                return len(psi.get_plugin_list())
+                            case "server_pid":
+                                return psi.get_server_pid()
+                            case "server_version":
+                                return psi.get_server_information().version
---
+++ mcdr_control_api/entry.py
@@ -0,0 +1,66 @@
+"""Default event listeners for MCDReforged to call.
+"""
+from mcdreforged.api.all import \
+    PluginServerInterface, CommandSource
+from mcdreforged.api.command import CommandContext
+from mcdreforged.api.command import SimpleCommandBuilder
+from mcdreforged.api.command import Boolean
+from mcdr_control_api import app
+
+import mcdr_control_api
+
+
+builder = SimpleCommandBuilder()
+fastapi_mcdr = None  # pylint: disable=invalid-name
+"""Shared instance of the fastapi_mcdr plugin.
+"""
+
+
+def on_load(server: PluginServerInterface, prev_module):  # pylint: disable=unused-argument
+    """Called when the plugin is loaded.
+    """
+    global fastapi_mcdr  # pylint: disable=global-statement
+    mcdr_control_api.psi = server
+    mcdr_control_api.app_id = server.get_self_metadata().id
+    server.logger.info("on_load")
+    builder.arg('use_logger', Boolean)
+    builder.register(server)
+    fastapi_mcdr = server.get_plugin_instance('fastapi_mcdr')
+    if fastapi_mcdr is not None and fastapi_mcdr.is_ready():
+        server.logger.info("on_load.start")
+        mount_app()
+    server.register_event_listener(
+        fastapi_mcdr.COLLECT_EVENT,  # type: ignore
+        mount_app
+    )
+
+
+def on_unload(server: PluginServerInterface):  # pylint: disable=unused-argument
+    """Called when the plugin is unloaded.
+    """
+    if fastapi_mcdr:
+        if mcdr_control_api.app_id:
+            fastapi_mcdr.unmount(mcdr_control_api.app_id)
+
+
+def mount_app():
+    """Called when the app will be mounted by fastapi_mcdr.
+    """
+    if fastapi_mcdr:
+        fastapi_mcdr.mount(mcdr_control_api.app_id, app)
+
+@builder.command('!!ctl_api print <use_logger>')
+def on_command_node_print(src: CommandSource, ctx: CommandContext):  # pylint: disable=unused-argument
+    """Debug command to print something need test.
+
+    Arguments:
+        <use_logger> - Whether to use mcdr logger or not.
+    """
+    server = mcdr_control_api.psi
+    if not isinstance(server, PluginServerInterface):
+        return
+    result = str(server.get_self_metadata().version)
+    if ctx['use_logger']:
+        server.logger.info(result)
+    else:
+        print(result)
